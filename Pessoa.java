5f40e69aac2265ea3285cf5862b078b62d4927a5
JotasRx <134642522+JotasRx@users.noreply.github.com>
2025-03-18 20:20:27 -0300

    Projeto 1

--- Pessoa.java
+++ Pessoa.java
@@ -1,46 +1,45 @@
-import java.util.List;
+
 
 public class Pessoa {
-    // Atributos com diferentes tipos de dados
     private String nome;
     private int idade;
-    private double altura;
-    private List<String> hobbies;
-
-    // Construtor para inicializar os atributos
-    public Pessoa(String nome, int idade, double altura, List<String> hobbies) {
-        this.nome = nome;
-        this.idade = idade;
-        this.altura = altura;
-        this.hobbies = hobbies;
+    public int altura;
+    public String hobbies;
+    
+    public Pessoa ( String nm,int ida, int alt, String hob){
+        this.nome = nm;
+        this.idade = ida;
+        this.altura = alt;
+        this.hobbies = hob; 
+    
     }
-
-    // Método para exibir os dados armazenados
-    public void exibirDados() {
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade + " anos");
-        System.out.println("Altura: " + altura + " metros");
-        System.out.print("Hobbies: ");
-        for (String hobby : hobbies) {
-            System.out.print(hobby + " ");
-        }
-        System.out.println();
-    }
-
-    // Métodos de acesso (getters)
+    
     public String getNome() {
         return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public int getIdade() {
         return idade;
     }
 
-    public double getAltura() {
-        return altura;
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
-
-    public List<String> getHobbies() {
-        return hobbies;
-    }
+            // Método para exibir os dados da pessoa
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Altura: " + altura + " metros");
+        System.out.print("Hobbies: " + hobbies);
+       
+    }   
+    
 }
+
+    
+
+
--- Principal.java
+++ Principal.java
@@ -1,50 +1,11 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Principal {
-    // Método para inserir dados da pessoa
-    public static Pessoa inserirDados() {
-        Scanner scanner = new Scanner(System.in);
-
-        // Inserindo nome
-        System.out.print("Digite o nome: ");
-        String nome = scanner.nextLine();
-
-        // Inserindo idade
-        System.out.print("Digite a idade: ");
-        int idade = scanner.nextInt();
-
-        // Inserindo altura
-        System.out.print("Digite a altura (em metros): ");
-        double altura = scanner.nextDouble();
-
-        // Limpando o buffer do scanner
-        scanner.nextLine();  // Para capturar a próxima linha corretamente
-
-        // Inserindo hobbies
-        List<String> hobbies = new ArrayList<>();
-        System.out.println("Digite os hobbies (digite 'sair' para terminar): ");
-        while (true) {
-            String hobby = scanner.nextLine();
-            if (hobby.equalsIgnoreCase("sair")) {
-                break;
-            }
-            hobbies.add(hobby);
-        }
-
-        // Criando e retornando a pessoa com os dados inseridos
-        return new Pessoa(nome, idade, altura, hobbies);
-    }
 
     public static void main(String[] args) {
-        // Inserindo os dados da pessoa
-        Pessoa pessoa1 = inserirDados();
-
-        // Exibindo os dados da pessoa
-        pessoa1.exibirDados();
-    }
-}
- 
     
-
+        Pessoa pessoa = new Pessoa("Jose", 25, 185, "futebol");
+        
+        pessoa.exibirDados();
+        
+    }   
+}
